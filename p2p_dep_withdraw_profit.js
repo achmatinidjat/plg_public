2790a7827dc162e66ca958de7e558fe8ce18ef13
krit <72283497+achmatinidjat@users.noreply.github.com>
2023-07-10 00:47:20 +0300

    Update p2p_dep_withdraw_profit.js

--- p2p_dep_withdraw_profit.js
+++ p2p_dep_withdraw_profit.js
@@ -1,1 +1,37 @@
-/* Wait a code!*/
+var amnt = document.querySelectorAll(".table_amount");
+var tpe = document.querySelectorAll(".table_prestrong");
+var stus = document.querySelectorAll(".table_status:not(.gray)");
+var ids = document.querySelectorAll(".padded")
+
+var ArrAll = Array();
+var ArrType = Array()
+for(var i = 0;i<stus.length;i++){
+    ArrAll.push(stus[i].textContent);
+}
+for(var i = 1; i<tpe.length;i+=3){
+    ArrType.push(tpe[i].textContent);
+}
+
+
+
+var withdraw = 0;
+var deposit = 0;
+for(var i = 0; i < ids.length; i++){
+    if((ArrType[i] == "DEPOSIT") && (ArrAll[i] == "Отменен")){
+        console.log(ArrType[i],">",ArrAll[i])
+        i++;
+    }else if((ArrType[i] == "WITHDRAW") && (ArrAll[i] == "Отменен")){
+        console.log(ArrType[i],">",ArrAll[i]);
+        i++;
+    }
+        if(ArrType[i] == "WITHDRAW" && ArrAll[i] == "Завершен"){
+        withdraw+=Number(amnt[i].textContent);
+
+    }else if(ArrType[i] == "DEPOSIT" && ArrAll[i] == "Завершен"){
+        deposit+=Number(amnt[i].textContent);
+    }
+}
+  
+console.log("withdraw >>",withdraw)
+console.log("deposit >>",deposit)
+console.log("Profit >>", withdraw - deposit)
